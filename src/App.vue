4b392495f4d87aa8e6ae6625677ac94e57f52e5e
Alan Gao <gtopia@163.com>
2018-01-17 19:04:19 +0800

    Add navbar.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    <img src="./common/img/logo.png">
+    <!-- <img src="./common/img/logo.png"> -->
+    <p class="nav">
+        <!-- 使用 router-link 组件来导航. -->
+        <!-- 通过传入 `to` 属性指定链接. -->
+        <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
+        <router-link to="/" class="item">Main</router-link>
+        <router-link to="/foo" class="item">Foo</router-link>
+        <router-link to="/bar" class="item">Bar</router-link>
+    </p>
     <router-view/>
   </div>
 </template>
@@ -11,13 +19,24 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+
+.nav {
+    margin-bottom: 60px;
+
+    .item {
+        margin-right: 10px;
+        text-decoration: underline;
+        text-decoration-color: blue;
+        -moz-text-decoration-color: blue;
+    }
 }
 </style>
