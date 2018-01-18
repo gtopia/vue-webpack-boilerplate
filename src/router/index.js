985860cb2f75447f8096e34ed0fa80dd2414fe1e
Alan Gao <gtopia@163.com>
2018-01-18 14:03:25 +0800

    Add embeded router.

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,14 @@ import HelloWorld from '@/components/helloWorld';
 Vue.use(Router);
 
 const User = {
-    template: '<div>User {{ $route.params.name }}</div>',
-    beforeRouteUpdate (to, from, next) {
+    // template: '<div>User {{ $route.params.id }}</div>',
+    template: `
+        <div class="user">
+            <h2>User {{ $route.params.id }}</h2>
+            <router-view></router-view>
+        </div>
+    `,
+    beforeRouteUpdate(to, from, next) {
         // react to route changes...
         // don't forget to call next()
         console.log('>> to: ', to);
@@ -15,16 +21,47 @@ const User = {
     }
 };
 
+const UserHome = {
+    template: '<div>User Home</div>'
+};
+
+const UserProfile = {
+    template: '<div>User Profile</div>'
+};
+
+const UserPosts = {
+    template: '<div>User Posts</div>'
+};
+
 export default new Router({
-    routes: [
-        {
+    routes: [{
             path: '/',
             name: 'HelloWorld',
             component: HelloWorld
         },
         {
-            path: '/user/:name',
-            component: User
+            path: '/user/:id',
+            component: User,
+            children: [
+                // 当 /user/:id 匹配成功，
+                // UserHome 会被渲染在 User 的 <router-view> 中
+                {
+                    path: '',
+                    component: UserHome
+                },
+                {
+                    // 当 /user/:id/profile 匹配成功，
+                    // UserProfile 会被渲染在 User 的 <router-view> 中
+                    path: 'profile',
+                    component: UserProfile
+                },
+                {
+                    // 当 /user/:id/posts 匹配成功
+                    // UserPosts 会被渲染在 User 的 <router-view> 中
+                    path: 'posts',
+                    component: UserPosts
+                }
+            ]
         }
     ]
 });
