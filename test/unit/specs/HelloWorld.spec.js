670bd86f00e1b058cfd178931941f2cecdb78029
Alan Gao <gtopia@163.com>
2018-01-18 11:45:05 +0800

    Syntex fixes.

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import HelloWorld from '@/components/helloWorld'
+import Vue from 'vue';
+import HelloWorld from '@/components/helloWorld';
 
 describe('HelloWorld.vue', () => {
     it('should render correct contents', () => {
         const Constructor = Vue.extend(HelloWorld);
         const vm = new Constructor().$mount();
         expect(vm.$el.querySelector('.hello h1').textContent).toEqual('Welcome to Vue.js App.');
-    })
-})
+    });
+});
