93d6c4c9030a6e02c819e30b2f23ebec1bd2d6d9
Alan Gao <gtopia@163.com>
2018-01-18 10:15:47 +0800

    Comma dangle fixes.

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ export default new Router({
             path: '/',
             name: 'HelloWorld',
             component: HelloWorld
-        },
+        }
         // {
         //     path: '/foo',
         //     component: Foo
